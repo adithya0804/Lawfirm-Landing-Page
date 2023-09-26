1ea6834faa45659e711bea4be157f427a9c4867d
adithya0804 <skumaradithya0804@gmail.com>
2023-09-26 11:49:01 +0530

    reworked

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -5,12 +5,12 @@ const HeroSection = () => {
   return (
     <div className={styles.wrapper}>
         <div className={styles.leftSection}>
-            <h1 className={styles.headerText}>You don’t have to 
-Fight them Alone.</h1>
+            <h1 className={styles.headerText}>You don’t have to </h1>
+            <h1 className={styles.headerText}>  Fight them Alone.</h1>
  <p className={styles.description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo repellat dolorum, labore numquam quasi recusandae sequi distinctio, quis vero saepe eaque cumque tenetur provident neque totam atque similique obcaecati fugit?</p>
         </div>
-      
-    </div>
+<div className={styles.imgContainer}></div>
+      </div>
   )
 }
 
