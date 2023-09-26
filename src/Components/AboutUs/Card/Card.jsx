e96d9e0191768e355b4616eead7db1aafc5a3664
adithya0804 <skumaradithya0804@gmail.com>
2023-09-26 13:36:36 +0530

    cardstyling

--- src/Components/AboutUs/Card/Card.jsx
+++ src/Components/AboutUs/Card/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({title}) => {
   return (
     <div className={styles.wrapper}>
         <div className={styles.imageWrapper}>
-        <img src={gift} alt="gift"/>
+        <img src={gift} alt="gift" className={styles.img}/>
         </div>
         <div className={styles.title}>{title}</div>
         <div className={styles.content}>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequatduis enim velit mollit Exer. </div>
