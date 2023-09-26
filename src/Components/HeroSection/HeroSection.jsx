b710c98002b610d7414f8ebf47237a072368d88f
adithya0804 <skumaradithya0804@gmail.com>
2023-09-26 12:16:16 +0530

    HeroSectioncompleted

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import styles from "./HeroSection.module.css"
+import SearchBar from '../SearchBar/SearchBar'
 
 const HeroSection = () => {
   return (
-    <div className={styles.wrapper}>
+    <div id="#home" className={styles.wrapper}>
         <div className={styles.leftSection}>
             <h1 className={styles.headerText}>You don’t have to </h1>
             <h1 className={styles.headerText}>  Fight them Alone.</h1>
  <p className={styles.description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo repellat dolorum, labore numquam quasi recusandae sequi distinctio, quis vero saepe eaque cumque tenetur provident neque totam atque similique obcaecati fugit?</p>
-        </div>
+      <SearchBar/>  </div>
 <div className={styles.imgContainer}></div>
       </div>
   )
